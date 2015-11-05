2b65efb63cdebe85206610f2cdd3c48a798d079a
iliantova <iliyana.antova@gmail.com>
2015-11-05 22:14:25 +0200

    name change

---
+++ TouristSitesSystem/Data/TouristSideSystem.Model/User.cs
@@ -0,0 +1,48 @@
+﻿namespace TouristSiteSystem.Model
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    public class User : IdentityUser
+    {
+        private ICollection<TouristSite> touristSites;
+
+        private ICollection<Image> images;
+
+        public User()
+        {
+            this.touristSites = new HashSet<TouristSite>();
+            this.images = new HashSet<Image>();
+        }
+
+        [MaxLength(25)]
+        public string FirstName { get; set; }
+
+        [MaxLength(25)]
+        public string LastName { get; set; }
+
+        public virtual ICollection<TouristSite> TouristSites
+        {
+            get { return this.touristSites; }
+            set { this.touristSites = value; }
+        }
+
+        public virtual ICollection<Image> Images
+        {
+            get { return this.images; }
+            set { this.images = value; }
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
---
+++ TouristSitesSystem/TouristSidesSystem.Api/Models/IdentityModels.cs
@@ -0,0 +1,11 @@
+﻿namespace TouristSitesSystem.Api.Models
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity.Owin;
+    using TouristSiteSystem.Model;
+
+    
+}
