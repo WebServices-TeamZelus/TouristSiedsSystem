290fb79f30ceedf13f83e856dd455a08def9249c
KYoveva <kristinayoveva@gmail.com>
2015-11-16 12:54:53 +0200

    Fixed Controllers

---
+++ TouristSitesSystem/TouristSitesSystem.Api/Models/AccomodationResponseModel.cs
@@ -0,0 +1,37 @@
+﻿namespace TouristSitesSystem.Api.Models
+{
+    using System;
+    using System.Linq.Expressions;
+    using TouristSiteSystem.Model;
+
+    public class AccomodationResponseModel
+    {
+        public static Expression<Func<Accomodation, AccomodationResponseModel>> FromModel
+        {
+            get
+            {
+                return a => new AccomodationResponseModel
+                {
+                    Name = a.Name,
+                    Description = a.Description,
+                    CityId = a.CityId,
+                    Adress = a.Adress,
+                    Email = a.Email,
+                    Mobile = a.Mobile
+                };
+            }
+        }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Email { get; set; }
+
+        public string Adress { get; set; }
+
+        public string Mobile { get; set; }
+
+        public int CityId { get; set; }
+    }
+}
